8f52e6c843fd17c185e72787bda2adfa38d811fe
Cameron Schmidt <cschmidt0121@gmail.com>
2024-07-10 14:41:13 -0400

    Even more incorrect widths

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hgtools',
-    version='1.2.3',
+    version='1.2.4',
     packages=find_packages(),
     py_modules=['hgtools'],
     install_requires=[
