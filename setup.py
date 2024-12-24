b884e3656470399cda8167816074ff236fe72fce
Cameron Schmidt <cschmidt0121@gmail.com>
2024-12-24 13:01:05 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hgtools',
-    version='1.4.2',
+    version='1.4.3',
     packages=find_packages(),
     py_modules=['hgtools'],
     install_requires=[
